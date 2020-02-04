61f263b4b905e5f2fb7d79f56f65ec8b36a4b902
Hans Hasselberg <me@hans.io>
2020-02-04 14:29:08 +0100

    add validations to ensure values are ok

--- agent/consul/server_test.go
+++ agent/consul/server_test.go
@@ -159,7 +159,7 @@ func testServerConfig(t *testing.T) (string, *Config) {
 			"RootCert":            "",
 			"RotationPeriod":      "2160h",
 			"LeafCertTTL":         "72h",
-			"IntermediateCertTTL": "72h",
+			"IntermediateCertTTL": "288h",
 		},
 	}
 
